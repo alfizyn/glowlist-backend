5ae6d6d19acc1eaf7d2bc0e36f5df4c7ca1a2394
alfizyn <alfi98239@gmail.com>
2026-08-06 13:52:05 +0700

    tambahan app pu dan delet

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require ('cors')
 const app = express();
 const mysql = require('mysql2');
 const PORT = 3001;
+
+app.use(cors())
+
 const db = mysql.createConnection({
     host: 'localhost',
     user:'root',
@@ -37,6 +41,61 @@ app.get('/', (req, res) => {
     db.query(sql, (err, results) => {
         if (err) returnres.status(500).json({ error: err });
         res.json(results);
+    });
+ });
+
+ app.post('/produk', (req, res) => {
+    const {judul, deskripsi, harga, id_kategori } = req.body;
+
+    if (!judul || !harga){
+        return res.status(400).json({ message: 'judul dan harga wajib diisi'});
+    }
+    if (!deskripsi) {
+        return res.status(400).json({message: 'Deskripsi waji diisi'});
+    }
+
+    const sql = 'INSERT INTO produk (judul, deskripsi, harga, id_kategori, tgl_input VALUES (?, ?, ?, ?, NOW())'; 
+    db.query(sql, [judul, deskripsi, harga, id_kategori], (err, results) => {
+        if (err) return res.status(500).json({ error: err.sqlMessage });
+        res.json({
+            message: 'Produk berhasil dimasukkan',
+            id_produ: results.insertId
+        });
+    });
+ });
+
+ app.put('/produk/:id_produk', (req, res) => {
+    const { id_produk } = req.params;
+    const { judul, deskripsi, harga, id_kategori } = req.body;
+
+    
+
+    const sql = 'UPDATE produk SET judul=?, deskripsi=?, harga=?, id_kategori=? WHERE id_produk=?';
+    db.query(sql, [judul, deskripsi, harga, id_kategori, id_produk], (err, results)=> {
+        if (err) {
+             return res.status(500).json({ error: err.sqlMessage });
+        
+
+        }
+        if (result.affectedRows === 0) {
+            return res.status(400).json({ message: 'Produk tidak ditemukan!' });
+        }
+             res.json({ message: 'Produk berhasil diupdate!' });
+    });
+
+ });
+
+ app.delete('/produk/:id_produk', (req, res) => {
+    const { id_produk } = req.params;
+    const sql = 'DELETE FROM produk WHERE id_produk = ?';
+    db.query(sql, [id_produk], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: err.sqlMessage });
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Produk tidak ditemukan!' });
+        }
+        res.json({ message: 'Produk berhasil dihapus!' });
     });
  });
 
