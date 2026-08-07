d3ebe5941319f6f9478d368d281a1fa0fb564060
alfizyn <alfi98239@gmail.com>
2026-08-07 13:40:07 +0700

    tambah app.post

--- server.js
+++ server.js
@@ -1,105 +1,210 @@
 const express = require('express');
-const cors = require ('cors')
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql2');
+
 const PORT = 3001;
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user:'root',
-    password:'',
-    database:'glowlist_db'
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
 });
 
-db.connect(err => {
-    if(err) {
+db.connect((err) => {
+    if (err) {
         console.error('Gagal konek ke database:', err);
-    }   else {
+    } else {
         console.log('Berhasil konek ke GlowList');
     }
 });
 
 
-app.use(express.json());
-
 app.get('/', (req, res) => {
     res.send('Selamat Datang di Glowlist API 💄');
 });
 
- app.get('/produk',(req, res) =>{
+app.get('/produk', (req, res) => {
     const sql = 'SELECT * FROM produk';
+
     db.query(sql, (err, results) => {
-        if (err) returnres.status(500).json({ error: err });
+        if (err) {
+            return res.status(500).json({ error: err });
+        }
+
         res.json(results);
     });
- });
-
- app.get('/kategori',(req, res) =>{
-    const sql = 'SELECT * FROM kategori';
-    db.query(sql, (err, results) => {
-        if (err) returnres.status(500).json({ error: err });
-        res.json(results);
-    });
- });
-
- app.post('/produk', (req, res) => {
-    const {judul, deskripsi, harga, id_kategori } = req.body;
-
-    if (!judul || !harga){
-        return res.status(400).json({ message: 'judul dan harga wajib diisi'});
-    }
-    if (!deskripsi) {
-        return res.status(400).json({message: 'Deskripsi waji diisi'});
-    }
-
-    const sql = 'INSERT INTO produk (judul, deskripsi, harga, id_kategori, tgl_input VALUES (?, ?, ?, ?, NOW())'; 
-    db.query(sql, [judul, deskripsi, harga, id_kategori], (err, results) => {
-        if (err) return res.status(500).json({ error: err.sqlMessage });
-        res.json({
-            message: 'Produk berhasil dimasukkan',
-            id_produ: results.insertId
-        });
-    });
- });
-
- app.put('/produk/:id_produk', (req, res) => {
-    const { id_produk } = req.params;
-    const { judul, deskripsi, harga, id_kategori } = req.body;
-
-    
-
-    const sql = 'UPDATE produk SET judul=?, deskripsi=?, harga=?, id_kategori=? WHERE id_produk=?';
-    db.query(sql, [judul, deskripsi, harga, id_kategori, id_produk], (err, results)=> {
-        if (err) {
-             return res.status(500).json({ error: err.sqlMessage });
-        
-
-        }
-        if (result.affectedRows === 0) {
-            return res.status(400).json({ message: 'Produk tidak ditemukan!' });
-        }
-             res.json({ message: 'Produk berhasil diupdate!' });
-    });
-
- });
-
- app.delete('/produk/:id_produk', (req, res) => {
-    const { id_produk } = req.params;
-    const sql = 'DELETE FROM produk WHERE id_produk = ?';
-    db.query(sql, [id_produk], (err, result) => {
-        if (err) {
-            return res.status(500).json({ error: err.sqlMessage });
-        }
-        if (result.affectedRows === 0) {
-            return res.status(404).json({ message: 'Produk tidak ditemukan!' });
-        }
-        res.json({ message: 'Produk berhasil dihapus!' });
-    });
- });
-
-app.listen(PORT, () =>{
-    console.log(`Server Glowlist jalan di http://localhost:${PORT}`);
 });
 
+app.get('/produk/:id_produk', (req, res) => {
+    const { id_produk } = req.params;
+
+    const sql = 'SELECT * FROM produk WHERE id_produk = ?';
+
+    db.query(sql, [id_produk], (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err });
+        }
+
+        res.json(results);
+    });
+});
+
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err });
+        }
+
+        res.json(results);
+    });
+});
+
+app.post('/produk', (req, res) => {
+
+    const { judul, deskripsi, harga, id_kategori } = req.body;
+
+
+    if (!judul || !harga) {
+        return res.status(400).json({
+            message: "Judul dan harga wajib diisi"
+        });
+    }
+
+
+    const sql = `
+        INSERT INTO produk 
+        (judul, deskripsi, harga, id_kategori, tgl_input)
+        VALUES (?, ?, ?, ?, NOW())
+    `;
+
+
+    db.query(
+        sql,
+        [judul, deskripsi, harga, id_kategori],
+        (err, results) => {
+
+            if (err) {
+                return res.status(500).json({
+                    error: err.sqlMessage
+                });
+            }
+
+
+            res.json({
+                message: "Produk berhasil dimasukkan",
+                id_produk: results.insertId
+            });
+
+        }
+    );
+
+});
+   
+
+app.put('/produk/:id_produk', (req, res) => {
+
+    const { id_produk } = req.params;
+
+    const { judul, deskripsi, harga, id_kategori } = req.body;
+
+
+    const sql = `
+        UPDATE produk 
+        SET judul=?, deskripsi=?, harga=?, id_kategori=?
+        WHERE id_produk=?
+    `;
+
+
+    db.query(
+        sql,
+        [judul, deskripsi, harga, id_kategori, id_produk],
+        (err, results) => {
+            if (err) {
+                return res.status(500).json({
+                    error: err.sqlMessage
+                });
+            }
+
+            if (results.affectedRows === 0) {
+                return res.status(404).json({
+                    message: 'Produk tidak ditemukan!'
+                });
+            }
+
+            res.json({
+                message: 'Produk berhasil diupdate!'
+            });
+
+        }
+    );
+
+});
+
+app.delete('/produk/:id_produk', (req, res) => {
+
+    const { id_produk } = req.params;
+    const sql = 'DELETE FROM produk WHERE id_produk = ?';
+    
+    db.query(sql, [id_produk], (err, result) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.sqlMessage
+            });
+        }
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({
+                message: 'Produk tidak ditemukan!'
+            });
+        }
+
+
+        res.json({
+            message: 'Produk berhasil dihapus!'
+        });
+ });
+});
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+app.post('/pengguna', async (req, res) => {
+    const { nama, email, password, no_hp } = req.body;
+
+    if (!nama || !email || !password) {
+        return res.status(400).json({ message: 'Nama, email, dan password wajib diisi' });
+    }
+    try {
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const sql = 'INSERT INTO pengguna (nama, email, password, no_hp) VALUES (?, ?, ?, ?)';
+        db.query(sql, [nama, email, hashedPassword, no_hp], (err,result) => {
+            if (err) {
+                if (err.code === "ER_DUP_ENTRY") {
+                    return res.status(400).json({
+                        message: "Email sudah terdaftar, gunakan email"
+                    });
+                }
+                return res.status(500).json({
+                    error: err.sqlMessage
+                });
+            }
+            res.json({
+                message: "Akun berhsil dibuat",
+                id_pengguna: result.insertId
+            });
+        });
+        } catch (err) {
+        res.status(500).json({ error: 'Gagal mengenkripsi password' });
+    }
+                
+    });
+app.listen(PORT, () => {
+    console.log(`Server Glowlist jalan di http://localhost:${PORT}`);
+});
